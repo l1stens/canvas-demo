88674b500f713af3519ea223630077c9f23df248
yang <yangd1020@gmail.com>
2019-02-21 17:26:41 +0800

    移动端适配

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var context = canvas.getContext('2d');
 
 autoSetCanvasSize(canvas)
 
-listenToMouse(canvas)
+listenToUser(canvas)
 
 
 var eraserEnabled = false
@@ -53,14 +53,49 @@ function drawLine(x1, y1, x2, y2) {
     context.closePath()
 }
 
-function listenToMouse(canvas) {
-
-
+function listenToUser(canvas) {
     var using = false
     var lastPoint = {
         x: undefined,
         y: undefined
     }
+if (document.body.ontouchstart !== undefined) {
+    //触屏设备
+    canvas.ontouchstart = function (aaa) {
+        var x = aaa.touches[0].clientX
+        var y = aaa.touches[0].clientY
+        using = true
+        if (eraserEnabled) {
+            context.clearRect(x - 5, y - 5, 10, 10)
+        } else {
+            lastPoint = {
+                "x": x,
+                "y": y
+            }
+        }
+    }
+    canvas.ontouchmove= function (aaa) {
+        var x = aaa.touches[0].clientX
+        var y = aaa.touches[0].clientY
+
+        if (!using) {return}
+
+        if (eraserEnabled) {
+            context.clearRect(x - 5, y - 5, 10, 10)
+        } else {
+            var newPoint = {
+                "x": x,
+                "y": y
+            }
+            drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+            lastPoint = newPoint
+        }
+    }
+    canvas.ontouchend = function (aaa) {
+        using = false
+    }
+} else {
+    //非触屏设备
     canvas.onmousedown = function(aaa) {
         var x = aaa.clientX
         var y = aaa.clientY
@@ -95,4 +130,6 @@ function listenToMouse(canvas) {
     canvas.onmouseup = function(aaa) {
         using = false
     }
+}
+
 }
